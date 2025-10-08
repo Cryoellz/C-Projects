de175894911baf18ad52f70f1967e18ae7ef7561
Yuan Dominique Biong <yuandomdom13@gmail.com>
2025-10-08 09:31:45 +0800

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,1 +1,23 @@
-sumple calculator in c
+#include <stdio.h>
+
+int main() {
+float num1, num2;
+ 
+      printf("Enter your desired first number: ");
+      scanf("%f", &num1);
+      
+      printf("Enter your desired second number: ");
+      scanf("%f", &num2);
+ 
+   float sum = num1 + num2;
+   float difference = num1 - num2;
+   float product = num1 * num2;
+   float quotient = num1 / num2;
+    
+    printf("Sum: %.2f\n", sum);
+    printf("Difference: %.2f\n", difference);
+    printf("Product: %.2f\n", product);
+    printf("Quotient: %.2f\n", quotient);
+        
+    return 0;
+}
